f76ea3261792cdbf99a8763199e2dabe499076e7
Ethan Hetrick <rqu4@cdc.gov>
2025-11-04 16:59:56 -0500

    refactor

--- bin/download_sal_genomes.sh
+++ bin/download_sal_genomes.sh
@@ -4,6 +4,7 @@ conda activate $HOME/my_conda_envs/ncbi
 
 cd /scratch/salmonella
 
+# Download dehydrated dataset
 datasets download genome taxon "Salmonella" \
     --dehydrated \
     --filename ./salmonella_dataset.zip \
@@ -11,9 +12,15 @@ datasets download genome taxon "Salmonella" \
     --assembly-version 'latest' \
     --api-key "${API_KEY}"
 
+# Unzip and rehydrate
 unzip ./salmonella_dataset.zip
 
 datasets rehydrate \
     --directory ./ \
     --no-progressbar \
     --api-key "${API_KEY}"
+
+# Download genome summary and convert to csv
+datasets summary genome taxon "Salmonella" > salmonella_dataset.json
+
+bin/datasets_summary2csv.py --json_file_path salmonella_dataset.json csv_file_path salmonella_dataset.csv
--- bin/download_sal_metadata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-conda activate $HOME/my_conda_envs/ncbi
-
-datasets summary genome taxon "Salmonella" > salmonella_dataset.json
